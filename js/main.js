c54df513e90e27f5e9d6f6aaec14c682dd67584a
ahmad <ahmad.bedja-boana@etud.univ-montp2.fr>
2014-10-09 00:54:48 +0200

    Header presque terminé

--- js/main.js
+++ js/main.js
@@ -1,1 +1,10 @@
+$(document).ready(function() {
 
+	var $container = $('#projects-grid');
+	// init
+	$container.isotope({
+		// options
+		itemSelector: '.iso-item',
+		layoutMode: 'masonry'
+	});
+});
