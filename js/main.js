85fd2462a8119eedf8f3b2ce3e21d76c22eadad9
ahmad <ahmad.bedja-boana@etud.univ-montp2.fr>
2014-10-29 00:41:17 +0100

    navbar push content of sibling (hardcoded), style of the toggle

--- js/main.js
+++ js/main.js
@@ -1,15 +1,9 @@
 $(document).ready(function() {
 
-	var cpt = 0;
-	$('.frise-item').viewportChecker({
-		classToAdd: 'front', // Class to add to the elements when they are visible
-		offset: 200, 
-		repeat: true, // Add the possibility to remove the class if the elements are not visible
-		// callbackFunction: function(elem, action){
-		// 	var index = $(elem).index();
-		// 	$(elem).css("z-index",index);
-		// 	$(elem).prev().css("z-index",cpt++);
-		// 	$(elem).prev().css("background-color",red);
-		// }
+	$('#page-header .navbar-collapse').on('show.bs.collapse', function () {
+		$("#apropos").css("padding-top","+=220" );
+	});
+	$('#page-header .navbar-collapse').on('hide.bs.collapse', function () {
+		$("#apropos").css("padding-top","-=220" );
 	});
 });
