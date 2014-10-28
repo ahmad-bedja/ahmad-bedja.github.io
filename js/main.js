dfcbaf68f9af70f0be5cf55ddf517c964e439cbd
ahmad <ahmad.bedja-boana@etud.univ-montp2.fr>
2014-10-28 17:14:55 +0100

    frise-items les uns sur les autres

--- js/main.js
+++ js/main.js
@@ -1,4 +1,15 @@
 $(document).ready(function() {
 
-	
+	var cpt = 0;
+	$('.frise-item').viewportChecker({
+		classToAdd: 'front', // Class to add to the elements when they are visible
+		offset: 200, 
+		repeat: true, // Add the possibility to remove the class if the elements are not visible
+		// callbackFunction: function(elem, action){
+		// 	var index = $(elem).index();
+		// 	$(elem).css("z-index",index);
+		// 	$(elem).prev().css("z-index",cpt++);
+		// 	$(elem).prev().css("background-color",red);
+		// }
+	});
 });
