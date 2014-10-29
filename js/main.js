e56d17441fd042c124be2a820f066d4c0cb3c802
ahmad <ahmad.bedja-boana@etud.univ-montp2.fr>
2014-10-29 12:47:03 +0100

    scrollspy and navbar affix

--- js/main.js
+++ js/main.js
@@ -1,9 +1,32 @@
 $(document).ready(function() {
 
-	$('#page-header .navbar-collapse').on('show.bs.collapse', function () {
-		$("#apropos").css("padding-top","+=220" );
+	$('#navbar .navbar-collapse').on('show.bs.collapse', function () {
+		$("#apropos").css("padding-top","+="+$("#apropos").height() );
 	});
-	$('#page-header .navbar-collapse').on('hide.bs.collapse', function () {
-		$("#apropos").css("padding-top","-=220" );
+	$('#navbar .navbar-collapse').on('hide.bs.collapse', function () {
+		$("#apropos").css("padding-top",0 );
+	});
+
+	$('#navbar').affix({
+        offset: {
+            top: $('.container-header').height()+parseInt($('#navbar').css('margin-top'))
+        }
+    }); 
+
+	$('body').scrollspy({ target: '#navbar',offset: $('#navbar').height() });
+
+	// alert();
+	$("#navbar ul li a[href^='#']").on('click', function(e) {
+
+		// prevent default anchor click behavior
+		e.preventDefault();
+
+		// store hash
+		var hash = this.hash;
+
+		// animate
+		$('html, body').animate({
+			scrollTop: $(this.hash).offset().top
+		}, 300);
 	});
 });
