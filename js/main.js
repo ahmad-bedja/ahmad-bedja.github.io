234a74ced7b5146e0a489511e8538fb32bb66ac3
ahmad <ahmad.bedja-boana@etud.univ-montp2.fr>
2014-11-05 11:51:03 +0100

    add specific css rule to show the mail by default because hover isn't triggered

--- js/main.js
+++ js/main.js
@@ -1,9 +1,8 @@
 $(document).ready(function() {
 
+	// gestion de l'affichage de l'adresse mail affichée en bas de page
 	var $mail = $(".mail-col");
 	$mail.css({'top':parseInt($(".contact-item").css('height')) });
-	// $mail.hide();
-
 	$(".contact-item").hover(function(){
 		$mail.slideDown(200);
 	});
@@ -12,24 +11,24 @@ $(document).ready(function() {
 	});
 
 
+	// NAVBAR Affix
 	$('#navbar').affix({
         offset: {
             top: $('.container-header').height()+parseInt($('#navbar').css('margin-top'))
         }
     });
-
 	$("#navbar").on('affix.bs.affix', function(){
 		$(".section:first-of-type").css({"margin-top":"98px","transition":"none"});
 	});
-
 	$("#navbar").on('affix-top.bs.affix', function(){
 		$(".section:first-of-type").css({"margin-top":"0","transition":"none"});
 	});
 
+
 	$('body').scrollspy({ target: '#navbar',offset: $('#navbar').height() });
 
+	//scroll to anchor
 	$("#navbar ul li a[href^='#']").on('click', function(e) {
-
 		// prevent default anchor click behavior
 		e.preventDefault();
 
@@ -43,6 +42,7 @@ $(document).ready(function() {
 	});
 });
 
+//cacher la navbar quand un item est selectioné (mode mobile)
 $(document).on('click','.navbar-collapse.in',function(e) {
     if( $(e.target).is('a') ) {
         $(this).collapse('hide');
